38a28b3dcb477f3ea6a844412a7b6715da8d1bf3
karen <karenquispego97@gmail.com>
2025-03-12 15:44:17 +0100

    button conectado

--- app/src/main/java/com/example/proyecto2_app/IniciarSesion.java
+++ app/src/main/java/com/example/proyecto2_app/IniciarSesion.java
@@ -1,6 +1,9 @@
 package com.example.proyecto2_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,15 @@ public class IniciarSesion extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        Button btnEntrar = findViewById(R.id.btnEntrar );
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IniciarSesion.this, pagina_inicio.class);
+                startActivity(intent); // Inicia la nueva actividad
+            }
+        });
     }
 }
